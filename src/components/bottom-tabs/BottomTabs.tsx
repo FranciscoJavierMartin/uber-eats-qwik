38b0b9bda76c8e6cf857cb17bae92998463016bb
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-07-29 19:10:03 +0200

    Set active tab

--- src/components/bottom-tabs/BottomTabs.tsx
+++ src/components/bottom-tabs/BottomTabs.tsx
@@ -4,30 +4,42 @@ import {
   HiMagnifyingGlassOutline,
 } from '@qwikest/icons/heroicons';
 import './BottomTabs.scss';
+import { useLocation } from '@builder.io/qwik-city';
 
 export default component$(() => {
+  const location = useLocation();
+
   const tabs = [
     {
       id: 'home',
       icon: HiHomeOutline,
       title: 'Home',
+      url: '/tabs/home',
     },
     {
       id: 'search',
       icon: HiMagnifyingGlassOutline,
       title: 'Search',
+      url: '/tabs/search',
     },
   ];
 
   return (
     <div class='tabs'>
       {tabs.map((tab) => (
-        <div key={tab.id} class='tab'>
+        <a
+          key={tab.id}
+          class={{
+            tab: true,
+            'tab--active': location.url.pathname.startsWith(tab.url),
+          }}
+          href={tab.url}
+        >
           <div class='icon'>
             <tab.icon />
           </div>
           <div class='title'>{tab.title}</div>
-        </div>
+        </a>
       ))}
     </div>
   );
