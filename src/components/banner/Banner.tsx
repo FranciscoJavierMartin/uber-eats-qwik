d4dba53b1468d8110aa4f5ba9fc613987c82bd05
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-01 19:21:27 +0200

    Clean code

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,15 +1,15 @@
 import { component$ } from '@builder.io/qwik';
-// import Slider from '~/components/slider/Slider';
+import Carousel from '~/components/carousel/Carousel';
 import './Banner.scss';
-import Carousel from '../carousel/Carousel';
+
+import image1 from '../../assets/imgs/1.jpg';
+import image2 from '../../assets/imgs/2.jpg';
+import image3 from '../../assets/imgs/3.jpg';
 
 export default component$(() => {
   return (
     <div class='border-bottom'>
-      {/* <div class='slider'> */}
-        {/* <Slider></Slider> */}
-        <Carousel />
-      {/* </div> */}
+      <Carousel images={[image1, image2, image3]} />
     </div>
   );
 });
--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,19 +1,24 @@
 import { component$ } from '@builder.io/qwik';
 import './Carousel.scss';
-import image1 from '../../assets/imgs/1.jpg';
-import image2 from '../../assets/imgs/2.jpg';
-import image3 from '../../assets/imgs/3.jpg';
 
-export default component$(() => {
-  const images = [image1, image2, image3];
-
+interface CarouselProps {
+  images: any[];
+}
+// TODO: Add autoPlay
+export default component$((props: CarouselProps) => {
   return (
     <div class='carousel'>
-      {images.map((image, index) => (
+      {props.images.map((image, index) => (
         <article key={index} class='carousel__card'>
-          <img src={image} class='carousel__card__poster' />
+          <img
+            src={image}
+            class='carousel__card__poster'
+            width={305}
+            height={170}
+          />
         </article>
       ))}
+      {/* TODO: Add pagination */}
     </div>
   );
 });
