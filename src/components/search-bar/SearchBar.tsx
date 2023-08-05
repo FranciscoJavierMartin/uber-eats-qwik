b56c43614ade0df0a1c64228076b59687c8bfd20
FranciscoJavierMartin <javiermartincuenca@gmail.com>
2023-08-05 19:40:45 +0200

    Track input

--- src/components/search-bar/SearchBar.tsx
+++ src/components/search-bar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal, useTask$ } from '@builder.io/qwik';
 import {
   HiMagnifyingGlassOutline,
   HiXMarkOutline,
@@ -6,10 +6,26 @@ import {
 import './SearchBar.scss';
 
 export default component$(() => {
+  const query = useSignal<string>('');
+
+  useTask$(async ({ track }) => {
+    const searchInput = track(() => query.value);
+    const controller = new AbortController();
+
+    console.log(query.value);
+    if (searchInput) {
+      // await debounceGetRestaurants(query.value, controller);
+    }
+
+    return () => {
+      controller.abort();
+    };
+  });
+
   return (
     <header class='header'>
       <HiMagnifyingGlassOutline />
-      <input placeholder='Search' />
+      <input placeholder='Search' bind:value={query} />
       <HiXMarkOutline />
     </header>
   );
